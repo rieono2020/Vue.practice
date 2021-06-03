1a7f72a9cd499b3946cfd1ccda764ac1283867e8
RIE ONO <rieono2020@gmail.com>
2021-06-03 09:38:52 +0900

    Vue.jsの練習

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    {{users}}
+  </div>
+</template>
+
+<script>
+const axios = require('axios')
+let url = 'https://jsonplaceholder.typicode.com/users'
+
+export default {
+  asyncData ({params}) {
+    return axios.get(url)
+      .then((res) => {
+        return { users : res.data }
+      })
+  }
+}
+</script>
+
+<style>
+
+</style>
